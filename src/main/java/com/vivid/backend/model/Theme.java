5c265a8f0462cc66119795f075edb62e8521dafa
Aidan Murray <aidanhansm@gmail.com>
2021-02-22 11:57:16 -0700

    Add Theme model
    Co-authored-by: Taylor Phillips <62676321+taphill@users.noreply.github.com>

---
+++ src/main/java/com/vivid/backend/model/Theme.java
@@ -0,0 +1,43 @@
+package com.vivid.backend.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "themes")
+public class Theme {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  private String title;
+
+  protected Theme() {
+  }
+
+  public Theme(User user, String title) {
+    this.user = user;
+    this.title = title;
+  }
+
+  public long getId() {
+    return this.id;
+  }
+
+  public User getUser() {
+    return this.user;
+  }
+
+  public String getTitle() {
+    return this.title;
+  }
+}
