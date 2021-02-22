fe71c74022aeee9cd20c86beeda6cef2cfe3eb4c
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-22 12:15:50 -0600

    Update seed file
    Co-Authored-By: Aidan Murray <48848183+TeknoServal@users.noreply.github.com>

--- src/main/java/com/vivid/backend/SeedDatabase.java
+++ src/main/java/com/vivid/backend/SeedDatabase.java
@@ -2,6 +2,8 @@ package com.vivid.backend;
 
 import com.vivid.backend.model.User;
 import com.vivid.backend.repository.UserRepository;
+import com.vivid.backend.model.Dream;
+import com.vivid.backend.repository.DreamRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,11 +17,17 @@ class SeedDatabase {
   private static final Logger log = LoggerFactory.getLogger(SeedDatabase.class);
 
   @Bean
-  CommandLineRunner initDatabase(UserRepository repository) {
+  CommandLineRunner initDatabase(UserRepository userRepository, DreamRepository dreamRepository) {
+    dreamRepository.deleteAll();
+    userRepository.deleteAll();
 
     return args -> {
-      log.info("Preloading " + repository.save(new User("Mike Jones", "mjones@example.com")));
-      log.info("Preloading " + repository.save(new User("Ava Drew", "adrew@example.com")));
+      User user1 = userRepository.save(new User("Mike Jones", "mjones@example.com"));
+      User user2 = userRepository.save(new User("Ava Drew", "adrew@example.com"));
+
+      Dream dream1 = dreamRepository.save(new Dream("02/22/2021", "Cool Dream", "This is a good dream", "Happy", user1));
+      Dream dream2 = dreamRepository.save(new Dream("02/23/2021", "Bad Dream", "This was scary", "Bad", user1));
+      Dream dream3 = dreamRepository.save(new Dream("02/22/2021", "Weird Dream", "This was very weird", "Fun", user2));
     };
   }
 }
