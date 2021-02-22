40cde495a166bd070e4bb4c3bab2d397187e0dc8
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-22 16:13:46 -0600

    Add relationships to the Dream and Theme models

--- src/main/java/com/vivid/backend/model/Dream.java
+++ src/main/java/com/vivid/backend/model/Dream.java
@@ -1,5 +1,7 @@
 package com.vivid.backend.model;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -29,6 +32,9 @@ public class Dream {
   private String description;
 
   private String emotion;
+
+  @OneToMany(mappedBy = "dream")
+  private Set<DreamTheme> dream_themes;
 
   protected Dream() {}
 
--- src/main/java/com/vivid/backend/model/Theme.java
+++ src/main/java/com/vivid/backend/model/Theme.java
@@ -1,11 +1,14 @@
 package com.vivid.backend.model;
 
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +23,9 @@ public class Theme {
   private User user;
 
   private String title;
+
+  @OneToMany(mappedBy = "theme")
+  private Set<DreamTheme> dream_themes;
 
   protected Theme() {
   }
