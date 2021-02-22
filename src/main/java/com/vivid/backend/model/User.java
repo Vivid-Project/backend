20e11e30a7ae700eda30ae730110b0f65016b5ed
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-21 23:08:09 -0600

    Update user model

--- src/main/java/com/vivid/backend/model/User.java
+++ src/main/java/com/vivid/backend/model/User.java
@@ -1,5 +1,9 @@
 package com.vivid.backend.model;
 
+import java.util.Base64;
+import java.security.SecureRandom;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -7,20 +11,26 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "users")
+@Table(name="users")
 public class User {
 
   @Id
   @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
+
   private String name;
+
+  @Column(unique=true)
   private String email;
+
+  private String token;
 
   protected User() {}
 
   public User(String name, String email) {
     this.name = name;
     this.email = email;
+    this.token = generateNewToken();
   }
 
   public Long getId() {
@@ -33,5 +43,18 @@ public class User {
 
   public String getEmail() {
     return this.email;
+  }
+
+  public String getToken() {
+    return this.token;
+  }
+
+  private static final SecureRandom secureRandom = new SecureRandom();
+  private static final Base64.Encoder base64Encoder = Base64.getUrlEncoder();
+
+  private static String generateNewToken() {
+      byte[] randomBytes = new byte[24];
+      secureRandom.nextBytes(randomBytes);
+      return base64Encoder.encodeToString(randomBytes);
   }
 }
--- src/main/java/com/vivid/backend/repository/UserRepository.java
+++ src/main/java/com/vivid/backend/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.vivid.backend.repository;
 import com.vivid.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
