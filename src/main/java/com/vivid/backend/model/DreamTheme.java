7543bf5cde5b7bd5dbb106c9c8b1b5c58d306682
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-22 18:07:14 -0600

    Add endpoints to user controller
    Co-Authored-By: Aidan Murray <48848183+TeknoServal@users.noreply.github.com>

--- src/main/java/com/vivid/backend/model/DreamTheme.java
+++ src/main/java/com/vivid/backend/model/DreamTheme.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class DreamTheme {
 
   @Id
-  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
 
   @ManyToOne
