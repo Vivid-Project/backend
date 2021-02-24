5214ca80b7cc9f06f8093b5179d07b56b74b0268
Aidan Murray <aidanhansm@gmail.com>
2021-02-23 17:35:10 -0700

    Add user and dream response filters
    Co-authored-by: Taylor Phillips <62676321+taphill@users.noreply.github.com>

---
+++ src/main/java/com/vivid/backend/filters/DreamFilters.java
@@ -0,0 +1,29 @@
+package com.vivid.backend.filters;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
+public class DreamFilters {
+  public static final FilterProvider DREAM_DEFAULT_FILTER = dreamDefaultFilter();
+  public static final FilterProvider DREAM_INCLUDE_USER_FILTER = dreamIncludeUserFilter();
+
+  private DreamFilters() {
+  }
+
+  private static FilterProvider dreamIncludeUserFilter() {
+    SimpleBeanPropertyFilter simpleBeanPropertyFilterDream = SimpleBeanPropertyFilter.serializeAllExcept();
+    SimpleBeanPropertyFilter simpleBeanPropertyFilterUser = SimpleBeanPropertyFilter.serializeAllExcept("token",
+        "dreams", "themes");
+
+    return new SimpleFilterProvider().addFilter("dreamFilter", simpleBeanPropertyFilterDream).addFilter("userFilter",
+        simpleBeanPropertyFilterUser);
+  }
+
+  private static FilterProvider dreamDefaultFilter() {
+    SimpleBeanPropertyFilter simpleBeanPropertyFilter = SimpleBeanPropertyFilter.serializeAllExcept("user");
+
+    return new SimpleFilterProvider().addFilter("dreamFilter", simpleBeanPropertyFilter);
+
+  }
+}
