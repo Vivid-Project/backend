1dd5c69afecbb8c00850e54d9542470976027a46
Aidan Murray <aidanhansm@gmail.com>
2021-02-23 17:33:54 -0700

    add methods to find dream and dreams by user id
    Co-authored-by: Taylor Phillips <62676321+taphill@users.noreply.github.com>

--- src/main/java/com/vivid/backend/repository/DreamRepository.java
+++ src/main/java/com/vivid/backend/repository/DreamRepository.java
@@ -1,8 +1,16 @@
 package com.vivid.backend.repository;
 
+import java.util.Optional;
+import java.util.Set;
+
 import com.vivid.backend.model.Dream;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DreamRepository extends JpaRepository<Dream, Long> {
+
+  public Set<Dream> findAllByUserId(Long userId);
+
+  public Optional<Dream> findByUserIdAndId(Long userId, Long dreamId);
 
 }
