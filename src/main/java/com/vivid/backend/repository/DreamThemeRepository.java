9c2734b362607366965a3494deb91677763881a7
Aidan Murray <aidanhansm@gmail.com>
2021-02-24 16:10:37 -0700

    Fix many-to-many relationship between dreams and themes
    Co-authored-by: Zach Stearns <64452036+Stearnzy@users.noreply.github.com>

--- src/main/java/com/vivid/backend/repository/DreamThemeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vivid.backend.repository;
-
-import com.vivid.backend.model.DreamTheme;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DreamThemeRepository extends JpaRepository<DreamTheme, Long> {
-
-}
