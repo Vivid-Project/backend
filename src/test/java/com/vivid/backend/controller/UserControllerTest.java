ae033a0382ffb1677e5f062d873a5d361911fc1b
Zach Stearns <zach.stearns92@gmail.com>
2021-02-27 13:47:25 -0700

    Start writing out test

--- src/test/java/com/vivid/backend/controller/UserControllerTest.java
+++ src/test/java/com/vivid/backend/controller/UserControllerTest.java
@@ -1,7 +1,42 @@
 package com.vivid.backend.controller;
 
+import org.apache.http.HttpStatus;
+import org.junit.Test;
+
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.*;
+
 public class UserControllerTest {
   
+  private String body = "{ \"email\": \"person@example.com\"}";
+  private String token = "";
+
+  @Before
+  public void setup() {
+    // Register
+    given()
+    .contentType(ContentType.JSON)
+    .body(body)
+    .when()
+    .post("https://vivid-project-backend.herokuapp.com/users/authenticate")
+    .then()
+    .assertThat()
+    .statusCode(HttpStatus.SC_OK);
+
+    // Generate Token 
+    token = given()
+    .contentType(ContentType.JSON)
+    .body(body)
+    .when()
+    .post("https://vivid-project-backend.herokuapp.com/users/authenticate")
+    .jsonPath()
+    .get("token");
+  }
+
+  public void testGetWithToken() {
+    given()
+  }
 }
 
 
