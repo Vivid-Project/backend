34b4d7cb1587133b2d3d54f18c2f94b786f82552
Taylor Phillips <taylorscottphillips@gmail.com>
2021-02-27 18:03:57 -0600

    Add findByUserAndDateBetween method

--- src/main/java/com/vivid/backend/repository/DreamRepository.java
+++ src/main/java/com/vivid/backend/repository/DreamRepository.java
@@ -7,6 +7,7 @@ import com.vivid.backend.model.Dream;
 import com.vivid.backend.model.User;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface DreamRepository extends JpaRepository<Dream, Long> {
 
@@ -18,4 +19,6 @@ public interface DreamRepository extends JpaRepository<Dream, Long> {
 
   public Optional<Dream> findByUserAndId(User user, Long dreamId);
 
+  @Query("SELECT d from Dream d WHERE d.user = ?1 and d.date >= ?2 and d.date <= ?3")
+  public Set<Dream> findByUserAndDateBetween(User user, String dateStart, String dateEnd);
 }
